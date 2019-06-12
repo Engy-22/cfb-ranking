32bc4be3ae7ee6fdf1117cc8ebd523144a857801
johnwilhite <jbwilhite@gmail.com>
2019-06-12 10:05:48 -0400

    restyling

--- public/js/app.js
+++ public/js/app.js
@@ -1,65 +1,66 @@
 
-
-// bootstrap the demo
 let app = new Vue({
-  el: '#app',
-  data() {
-    return {
-      files: [],
-      file: '',
-      rankings: {},
-      selectedTeam: false,
-      showAll: false
-    };
-  },
-  computed: {
-    season() {
-      return this.file.split('.')[0];
+    el: '#app',
+    data() {
+        return {
+            files: [],
+            file: '',
+            rankings: {},
+            selectedTeam: false,
+            showAll: false
+        };
     },
-    week() {
-      return this.file.split('.')[1];
-    }
-  },
-  async created() {
-    let response = await fetch(`public/rankings/index.json`)
-    this.files = await response.json();
-
-    // default to last file
-    this.file = this.files[this.files.length - 1];
-    this.getRankings();
-  },
-  methods: {
-    kebabCase(string) {
-      return string.replace(/\s+/g, '-').toLowerCase();
-    },
-    formatFilename(file) {
-      return `${file.split('.')[0]} Week ${file.split('.')[1]}`;
-    },
-    async getRankings() {
-      let response = await fetch(`public/rankings/${this.file}`);
-      this.rankings = await response.json();
-    },
-    getTeam(id) {
-      let team = this.rankings.find(team => team.id == id);
-      return team || {name: 'Non FBS'};
-    },
-    selectTeam(team) {
-      if (team.id) {
-        this.selectedTeam = team;
-      }
-    },
-    getRecord(weeks) {
-      let wins = 0;
-      let losses = 0;
-      weeks && weeks.forEach(week => {
-        if (week && week.w) {
-          wins++;
-        } else {
-          losses++;
+    computed: {
+        season() {
+            return this.file.split('.')[0];
+        },
+        week() {
+            return this.file.split('.')[1];
         }
-      });
+    },
+    async created() {
+        let response = await fetch(`public/rankings/index.json`)
+        this.files = await response.json();
 
-      return `${wins} - ${losses}`;
+        // default to last file
+        this.file = this.files[this.files.length - 1];
+        await this.getRankings();
+    },
+    methods: {
+        kebabCase(string) {
+            return string.replace(/\s+/g, '-').toLowerCase();
+        },
+        formatFilename(file) {
+            return `${file.split('.')[0]} Week ${file.split('.')[1]}`;
+        },
+        async getRankings() {
+            let response = await fetch(`public/rankings/${this.file}`);
+            this.rankings = await response.json();
+
+            // select the first team
+            this.selectTeam(this.rankings[0]);
+        },
+        getTeam(id) {
+            let team = this.rankings.find(team => team.id == id);
+            return team || {name: 'Non FBS'};
+        },
+        selectTeam(team) {
+            if (team.id) {
+            this.selectedTeam = team;
+            }
+        },
+        getRecord(weeks) {
+            let wins = 0;
+            let losses = 0;
+            weeks && weeks.forEach(week => {
+            if (week && week.w) {
+                wins++;
+            } else {
+                losses++;
+            }
+            });
+
+            return `${wins} - ${losses}`;
+        }
     }
-  }
 })
